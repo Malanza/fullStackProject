3373845a126c959a7069f5c75c05c8566b4c7c49
unknown <melvinlanza16@gmail.com>
2022-09-03 21:39:01 -0700

    updated changes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector('#btn');
 const body = document.querySelector('body');
+const workoutContainer = document.querySelector('#workout-container');
+const datesContainer = document.querySelector('#dates-container');
+const durationContainer = document.querySelector('#duration-container');
 getOne()
 
 function getOne(){
@@ -9,16 +12,36 @@ function getOne(){
 
 
 async function fetchData(){
-    const data = await fetch('http://localhost:3000/get');
+    const data = await fetch(`http://localhost:3000/get`);
     const json = await data.json()
-    const monday = json[0].workout;
-    const workouts = JSON.stringify(monday);
-    makeDivs(workouts)
+    const cardio = json;
+    for (let i = 0; i < cardio.length; i++) {
+        const workouts = cardio[i].workout;
+        const dates = cardio[i].date;
+        const time = cardio[i].duration;
+        workoutsDivs(workouts);
+        datesDivs(dates)
+        durationDivs(time)
+    }
 }
 
-function makeDivs(workouts){
+function workoutsDivs(element){
+    const workouts = document.createElement('div');
+    workouts.id = 'workouts';
+    workouts.append(element);
+    workoutContainer.append(workouts)
+}
+
+
+function datesDivs(element){
     const container = document.createElement('div');
-    container.id = 'container';
-    container.append(workouts);
-    body.append(container);
+    container.id = 'dates';
+    container.append(element);
+    datesContainer.append(container);
+}
+function durationDivs(element){
+    const container = document.createElement('div');
+    container.id = 'duration';
+    container.append(element);
+    durationContainer.append(container);
 }
